facf689ddee6b1514f027591c9671075327d8fa8
Pawan1306 <141865155+Pawan1306@users.noreply.github.com>
2023-09-16 10:59:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+  // Function to be executed when the DOM is loaded
+       <script> 
+		   function onDOMLoaded() {
+            // Find the <div> element with the id "output"
+            var outputDiv = document.getElementById("output");
+
+            // Check if the element exists
+            if (outputDiv) {
+                // Create a text node with the message
+                var textNode = document.createTextNode("DOM load success");
+
+                // Append the text node to the <div> element
+                outputDiv.appendChild(textNode);
+            }
+        }
+
+        // Add an event listener for the DOMContentLoaded event
+        document.addEventListener("DOMContentLoaded", onDOMLoaded);
+    </script>
