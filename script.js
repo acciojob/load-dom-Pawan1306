e21a8fd0133759f8fba3191827e37663097a90cd
Pawan1306 <141865155+Pawan1306@users.noreply.github.com>
2023-10-19 15:54:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
- function onDOMLoaded() {
+
+        function onDOMLoaded() {
             // Create a new text node with the message
             var textNode = document.createTextNode("DOM load success");
 
@@ -8,6 +9,10 @@
 
             // Append the <p> element to the body
             document.body.appendChild(paragraph);
+
+            // Remove this script element from the body
+            var scriptElement = document.querySelector("script");
+            scriptElement.parentNode.removeChild(scriptElement);
         }
 
         // Add an event listener for DOMContentLoaded
