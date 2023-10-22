6cdc50ff45dc1d1ae6f99e563e5daabde85bed64
Pawan1306 <141865155+Pawan1306@users.noreply.github.com>
2023-10-22 21:55:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
-        document.addEventListener("DOMContentLoaded");
+        document.addEventListener("DOMContentLoaded", function(){
+			
+	
             
             let body = document.getElementByTagName('body')[0];
 			body.textContent = 'Dom load success";
