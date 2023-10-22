dc2bdef94fa9538d07fb0e4da4f6b4c8d0aaca58
Pawan1306 <141865155+Pawan1306@users.noreply.github.com>
2023-10-22 21:52:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
 
-        function onDOMLoaded() {
-            // Create a new text node with the message
-            var textNode = document.createTextNode("DOM load success");
-
-            // Create a <p> element to contain the text node
-            var paragraph = document.createElement("p");
-            paragraph.appendChild(textNode);
-
-            // Append the <p> element to the body
-            document.body.appendChild(paragraph);
-
-            // Remove this script element from the body
-            var scriptElement = document.querySelector("script");
-            scriptElement.parentNode.removeChild(scriptElement);
-        }
-
-        // Add an event listener for DOMContentLoaded
-        document.addEventListener("DOMContentLoaded", onDOMLoaded);
+        document.addEventListener("DOMContentLoaded");
+            
+            let body = document.getElementByTagName('body')[0];
+			body.textContent = 'Dom load success";
+		});
 
